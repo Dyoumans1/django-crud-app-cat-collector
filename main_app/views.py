85a24ead330ba15dbabd41434c75d4bc95738c3c
Dyoumans <derrikyoumans416@gmail.com>
2025-04-01 14:40:50 -0400

    day two

--- main_app/views.py
+++ main_app/views.py
@@ -1,38 +1,22 @@
-# main_app/views.py
-
 from django.shortcuts import render
+from .models import Cat
 
-# Import HttpResponse to send text-based responses
 from django.http import HttpResponse
 
-# views.py
+# Create your views here.
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-
-
-
-# Define the home view function
 def home(request):
-    # Send a simple HTML response
-    return render(request, 'home.html')
+  return render(request, 'home.html')
+
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
 
 def cat_index(request):
-    
-    return render(request, 'cats/index.html', {'cats': cats})
+  cats = Cat.objects.all()
+  return render(request, 'cats/index.html', { 'cats': cats })
+
+def cat_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/detail.html', { 'cat': cat })
 
 
